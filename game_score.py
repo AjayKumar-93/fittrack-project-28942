4386f0faf7b58ac81b352a51c4961b4b35a1a047
AJAY KUMAR <kuruvagorantlaajaykumar@gmail.com>
2026-01-31 12:04:35 +0530

    Add files via upload
    Masai_Game_Score Task

---
+++ game_score.py
@@ -0,0 +1,8 @@
+Player=input("Enter player name: ")
+Games_Played=int(input("Enter the Games played: "))
+Score=int(input("Enter the toatl Score: "))
+Average=Score/Games_Played
+print(f"Player: {Player}")
+print(f"Games Played: {Games_Played}")
+print(f"Total Score: {Score}")
+print(f"Average: {Average}")
